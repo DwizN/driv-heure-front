fff457914822ba176a1c13a3b987bff093b25384
Petit-Chaperon-Rouge <axel.lecoeuche@sfr.fr>
2020-03-05 03:22:28 +0100

    Edit the Notification page

--- src/pages/Notifications.vue
+++ src/pages/Notifications.vue
@@ -8,92 +8,40 @@
           </md-card-header>
           <md-card-content>
             <div class="md-layout">
+                <div class="md-layout-item md-medium-size-100">
+                    <h5>Réservations</h5>
+                    <div class="alert alert-info">
+                        <button type="button" aria-hidden="true" class="close">
+                            ×
+                        </button>
+                        <span>Emilie D. a réservé une leçon de conduite de 16h à 18h30 le 18/02/2020 avec Gwen</span>
+                    </div>
+                    <div class="alert alert-info">
+                        <button type="button" aria-hidden="true" class="close">
+                            ×
+                        </button>
+                        <span>Mouad B. a réservé une leçon de conduite de 11h à 13h00 le 18/02/2020 avec Antoine</span>
+                    </div>
+                    <div class="alert alert-info">
+                        <button type="button" aria-hidden="true" class="close">
+                            ×
+                        </button>
+                        <span>Axel L. a réservé une leçon de conduite de 9h à 11h00 le 18/02/2020 avec Antoine</span>
+                    </div>
+                </div>
               <div class="md-layout-item md-medium-size-100">
-                <h5>Notifications Style</h5>
-                <div class="alert alert-info">
-                  <span>This is a plain notification</span>
-                </div>
-                <div class="alert alert-info">
-                  <button type="button" aria-hidden="true" class="close">
-                    ×
-                  </button>
-                  <span>This is a notification with close button.</span>
-                </div>
-                <div
-                  class="alert alert-info alert-with-icon"
-                  data-notify="container"
-                >
-                  <button type="button" aria-hidden="true" class="close">
-                    ×
-                  </button>
-                  <i data-notify="icon" class="material-icons">add_alert</i>
-                  <span data-notify="message"
-                    >This is a notification with close button and icon.</span
-                  >
-                </div>
-                <div
-                  class="alert alert-info alert-with-icon"
-                  data-notify="container"
-                >
-                  <button type="button" aria-hidden="true" class="close">
-                    ×
-                  </button>
-                  <i data-notify="icon" class="material-icons">add_alert</i>
-                  <span data-notify="message"
-                    >This is a notification with close button and icon and have
-                    many lines. You can see that the icon and the close button
-                    are always vertically aligned. This is a beautiful
-                    notification. So you don't have to worry about the
-                    style.</span
-                  >
-                </div>
-              </div>
-              <div class="md-layout-item md-medium-size-100">
-                <h5>Notification states</h5>
-                <div class="alert alert-info">
-                  <button type="button" aria-hidden="true" class="close">
-                    ×
-                  </button>
-                  <span
-                    ><b> Info - </b> This is a regular notification made with
-                    ".alert-info"</span
-                  >
-                </div>
-                <div class="alert alert-success">
-                  <button type="button" aria-hidden="true" class="close">
-                    ×
-                  </button>
-                  <span
-                    ><b> Success - </b> This is a regular notification made with
-                    ".alert-success"</span
-                  >
-                </div>
+                <h5>Important</h5>
                 <div class="alert alert-warning">
                   <button type="button" aria-hidden="true" class="close">
                     ×
                   </button>
-                  <span
-                    ><b> Warning - </b> This is a regular notification made with
-                    ".alert-warning"</span
-                  >
+                  <span>Axel L. ne s'est pas présenté à sa séance de conduite du 18/02/2020 prévue de 9h à 11h</span>
                 </div>
                 <div class="alert alert-danger">
                   <button type="button" aria-hidden="true" class="close">
                     ×
                   </button>
-                  <span
-                    ><b> Danger - </b> This is a regular notification made with
-                    ".alert-danger"</span
-                  >
-                </div>
-                <div class="alert alert-primary">
-                  <button type="button" aria-hidden="true" class="close">
-                    ×
-                  </button>
-                  <span
-                    ><b> Primary - </b> This is a regular notification made with
-                    ".alert-primary"</span
-                  >
+                  <span>Un accident a eu lieu lors de la séance de conduite du 17/02/2020 de 15h30 à 17h de Mathis R. accompagné par Antoine</span>
                 </div>
               </div>
             </div>
